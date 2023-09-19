0b1e95b3ba461cc4acf23837250d4446f6f21172
Ishan <koradiaishan335@gmail.com>
2023-09-19 13:09:33 +0530

    added indicator slug col

--- models/prod/indicators.sql
+++ models/prod/indicators.sql
@@ -6,12 +6,13 @@ select
     "indicator", 
     to_timestamp("month", 'MM/DD/YYYY') as "month", 
     cast(regexp_replace("measure", ',', '', 'g') as float) as measure,
-    CASE 
+    CASE "indicator"
     WHEN '# of service contacts at NGO partners clinics' THEN '# of service contacts'
     WHEN '# of youth (15-25 years) accessing reproductive health services (PBG Indicator, reported quarter basis)' THEN '# of youth'
     WHEN '# of newborns born in supported clinics and catchment area receiving immediate newborn care (within 72 hours)' THEN '# of newborns'
     WHEN '# of children less than 12 months of age who received DPT3/Penta3 from USG-supported programs' THEN '# of children'
     WHEN '# of injectables provided through USG supported program to prevent unintended pregnancies' THEN '# of injectables'
+    ELSE ''
     END as "indicator_slug",
     "quarter"
 from {{ ref('merged') }}
