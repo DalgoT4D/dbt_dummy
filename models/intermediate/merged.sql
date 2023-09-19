4394ea61af90ab7e174053b62d301efc596c5770
Ishan <koradiaishan335@gmail.com>
2023-09-19 12:54:41 +0530

    adding quarter info

--- models/intermediate/merged.sql
+++ models/intermediate/merged.sql
@@ -1,23 +1,21 @@
 {{ config(materialized="table", schema="intermediate") }}
 
+select
+    "Month" as "month",
+    "ngo",
+    "spoc",
+    "measure",
+    "Indicator" as "indicator",
+    "Q1" as "quarter"
+from {{ source("sheets", "sheet1") }}
+union all
+select
+    "Month" as "month",
+    "ngo",
+    "spoc",
+    "measure",
+    "Indicator" as "indicator",
+    "Q2" as "quarter"
+from {{ source("sheets", "sheet2") }}
 
-with
-    merged as (
-        select
-            "Month" as "month",
-            "ngo",
-            "spoc",
-            "measure",
-            "Indicator" as "indicator"
-        from {{ source("sheets", "sheet1") }}
-        union all
-        select
-            "Month" as "month",
-            "ngo",
-            "spoc",
-            "measure",
-            "Indicator" as "indicator"
-        from {{ source("sheets", "sheet2") }}
-    )
 
-    
