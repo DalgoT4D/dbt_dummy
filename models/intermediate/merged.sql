6526e19d007aea77473ee5fd9e54ffb251405e13
Ishan <koradiaishan335@gmail.com>
2023-09-19 11:10:29 +0530

    reading month column as datetime

--- models/intermediate/merged.sql
+++ models/intermediate/merged.sql
@@ -1,5 +1,17 @@
 {{ config(materialized="table", schema="intermediate") }}
 
-select * from {{ source("sheets", "sheet1") }}
+select
+    to_date("Month", 'MM/DD/YYYY') as "month",
+    "ngo",
+    "spoc",
+    "measure",
+    "Indicator" as "indicator"
+from {{ source("sheets", "sheet1") }}
 union all
-select * from {{ source('sheets', 'sheet2') }}
+select
+    to_date("Month", 'MM/DD/YYYY') as "month",
+    "ngo",
+    "spoc",
+    "measure",
+    "Indicator" as "indicator"
+from {{ source("sheets", "sheet2") }}
